43551f05d58b658b47a4eb6b8e575843938d982b
dixonryan <c27dixon.ryan@afacademy.af.edu>
2025-11-21 21:47:00 -0700

    doneski

--- pex6main.hs
+++ pex6main.hs
@@ -8,19 +8,18 @@
       bool in order to satisfy the left side of the equals sign
 -}
 
-{- TODOS:
-+Create unknot logic to check for missing tuple in the typeII knots
-+Make code recurse/loop until the entire knot it solved
--}
-
 unKnot :: [(Char, Char)] -> String
 unKnot tripCode
+   --a cross (idk if possible) means not solveable
    | getLength tripCode <= 1 = "unknot"
+   -- if type1 returns with missing tuples -> unknot rest of thing
    | typeIknot tripCode /= tripCode = unKnot (typeIknot tripCode)
-   -- typeIIfindPair
+   -- same logic as type1
+   | getLength(typeIIfindPair tripCode) == 4 = unKnot (removeTypeII tripCode (typeIIfindPair tripCode))
+   -- else there is a tangle and returns not untangled crosses
    | otherwise = "tangle - resulting trip code: " ++ (show tripCode)
 
-
+--gets length of list of tuples passed in
 getLength [] = 0
 getLength (x:xs) = 1 + getLength xs
 
@@ -32,7 +31,6 @@ typeIknot (x:y:xs)
    | fst(x) == fst(y) = typeIknot xs
    --head doesn't match second; bypass it and check rest of trip code
    | otherwise = x: typeIknot (y:xs)
-
 
 --no point in looking if a typeII not possible
 typeIIfindPair [] = []
@@ -50,19 +48,26 @@ typeIIfindThird [] _ = []
 --finds the 4th tuple (third thing to find)
 typeIIfindThird (x:xs) [p3]
    --returns the 4th thing if found
-   | fst(x) == fst(p3) && snd(x) /= snd(x) = [x]
+   | fst(x) == fst(p3) && snd(x) /= snd(p3) = [x]
    --otherwise keeps looking
    | otherwise = typeIIfindThird (xs) [p3]
-
-
 
 typeIIfindSecond [] _ = []
 --finds first occurence of either of the two found in the pair
 typeIIfindSecond (x:xs) [p1, p2]
    --if it finds the first pair with different ending it will look for the 4th tuple & append it
    | fst(x) == fst(p1) && snd(x) /= snd(p1) = p1: typeIIfindThird xs [p2]
+   | fst(x) == fst(p2) && snd(x) /= snd(p2) = p2: typeIIfindThird xs [p1]
    --otherwise it keeps looking
    | otherwise = typeIIfindSecond (xs) [p1, p2]
+
+--to remove the 4 tuples of a type 2 knot if found
+removeTypeII [] [a,b,c,d] = []
+removeTypeII (x:xs) [a,b,c,d]
+   --if x equals any of the tuples remove x
+   | x == a || x==b || x==c || x==d = removeTypeII xs [a,b,c,d]
+   -- else keep looking for other 4
+   | otherwise = removeTypeII xs [a,b,c,d]
 
 
 
@@ -71,11 +76,8 @@ main = do
    let t01 = [('a','o'),('a','u')]
    let t02 = [('a','o'), ('a','u'), ('b','u'), ('b','o')]
    let t03 = [('a','o'),('b','o'),('c','o'),('c','u'),('b','u'),('a','u')]
-   print("   test case t01 - tripcode: " )
-   print(t01)
-   --print("   result:" ++ unKnot t01)
+   let t04 = [('a','o'),('b','o'),('c','u'),('a','u'),('b','u'),('c','o')]
+   let t05 = [('a','o'),('b','u'),('c','u'),('d','o'),('d','u'),('a','u'),('b','o'),('e','u'),('f','o'),('g','o'),('h','u'),('f','u'),('g','u'),('h','o'),('e','o'),('c','o')]
+   let t06 = [('a','o'),('q','u'), ('a','u')]
 
-   --print(typeIknot(t01))
-   --print(typeIknot(t03))
-   print(unKnot t03)
-   --print(typeIIfindPair(t03))
+   print(unKnot(t06))
